f0c5c01b943830e9c386897d63a1e5ecfc1a2cdf
VIVIDRON <ronaks21205@gmail.com>
2025-01-16 21:11:11 +0530

    update layout

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,15 +1,96 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class WeatherScreen extends StatelessWidget{
+class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("hello world"),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Weather App',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            )),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: GestureDetector(
+              child: const Icon(Icons.refresh),
+              onTap: () {
+                // print("refresh");
+              },
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                color: const Color.fromARGB(255, 40, 38, 38),
+                elevation: 10,
+                shape : RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10 , sigmaY: 10),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            '303.5°F',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Icon(
+                            Icons.cloud,
+                            size: 64,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'Rain',
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
